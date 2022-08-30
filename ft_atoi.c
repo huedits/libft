a9230ead2a3cd102fbcbe52b01b68d463d77fa7f
vimatheu <vimatheu@student.42sp.org.br>
2022-08-31 01:05:40 +0200

    Automatic commit from Makefile

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vimatheu <vimatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 18:45:04 by vimatheu          #+#    #+#             */
-/*   Updated: 2022/08/30 16:43:39 by vimatheu         ###   ########.fr       */
+/*   Updated: 2022/08/31 01:04:57 by vimatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,48 @@ static int	ft_check_limit(int res, int c, int sig);
 int	ft_atoi(const char *c)
 {
 	unsigned int	i;
-	int				r;
-	int				s;
+	int				res;
+	int				sig;
 
 	i = 0;
-	r = 0;
-	if (c[0] == '-')
+	res = 0;
+	sig = 1;
+	while (c[i] == ' ' || c[i] == '\t' || c[i] == '\n'
+		|| c[i] == '\v' || c[i] == '\f' || c[i] == '\r')
+		i++;
+	if (c[i] == '+' || c[i] == '-')
 	{
-		s = -1;
+		if (c[i] == '-')
+			sig *= -1;
 		i++;
 	}
-	else
-		s = 1;
 	while (c[i])
 	{
-		if (!(c[i] >= '0' && c[i] <= '9'))
+		if (!ft_isdigit(c[i]))
 			return (0);
-		if (ft_check_limit(r, (c[1] - '0'), s) == 0)
+		if (ft_check_limit(res, (c[i] - '0'), sig) == 0)
 			return (0);
-		r = r * 10 + (c[i] - '0');
+		res = res * 10 + (c[i] - '0');
 		i++;
 	}
-	return (r * s);
+	return (res * sig);
 }
 
-static int	ft_check_limit(int res, int c, int sig)
+static int	ft_check_limit(int res, int nbr, int sig)
 {
 	if (sig > 0)
 	{
 		if (res > (~(1 << 31)) / 10)
 			return (0);
-		if ((res * 10) > ((~(1 << 31)) - c))
+		if ((res * 10) > ((~(1 << 31)) - nbr))
 			return (0);
 		return (1);
 	}
 	else
 	{
-		if (-res < (1 << 31) / 10)
+		if (res < (1 << 31) / 10)
 			return (0);
-		if ((-res * 10) < ((1 << 31) + c))
+		if ((res * 10) < ((1 << 31) + nbr))
 			return (0);
 		return (1);
 	}
