503e7fa8a226cfcedc5df9455d0e1d2cb42616ca
vimatheu <vimatheu@student.42sp.org.br>
2022-08-31 20:29:15 +0200

    Automatic commit from Makefile

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: vimatheu <vimatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 01:51:47 by vimatheu          #+#    #+#             */
-/*   Updated: 2022/08/30 01:55:54 by vimatheu         ###   ########.fr       */
+/*   Updated: 2022/08/31 20:26:56 by vimatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
+	if (c > 255)
+		return ((char *)s);
 	i = 0;
 	while (s[i])
 		i++;
-	while (s[i] != c && i >= 0)
+	while (s[i] != c && i > 0)
 		i--;
 	if (s[i] == c)
 		return ((char *) &s[i]);
