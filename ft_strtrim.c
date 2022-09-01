293284bb4bca6e7578ef597604915e5e1c948d69
vimatheu <vimatheu@student.42sp.org.br>
2022-09-02 00:10:44 +0200

    Automatic commit from Makefile

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: vimatheu <vimatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 22:42:31 by vimatheu          #+#    #+#             */
-/*   Updated: 2022/09/01 23:53:50 by vimatheu         ###   ########.fr       */
+/*   Updated: 2022/09/02 00:10:16 by vimatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	while (s1[start] && check_delim(s1[start], (char *)set))
 		start++;
-	end = ft_strlen(s1);
+	end = ft_strlen(s1) - 1;
 	while (end > start && check_delim(s1[end], (char *)set))
 		end--;
-	trim = ft_substr(s1, start, (end - start));
+	trim = ft_substr(s1, start, ((end - start) + 1));
 	if (!trim)
 		return (NULL);
 	return (trim);
