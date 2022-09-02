4130a286ccc9beb1972eb51e42c5cb480d5e31d5
vimatheu <vimatheu@student.42sp.org.br>
2022-09-02 20:30:40 +0200

    Automatic commit from Makefile

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: vimatheu <vimatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 19:19:27 by vimatheu          #+#    #+#             */
-/*   Updated: 2022/09/02 20:19:11 by vimatheu         ###   ########.fr       */
+/*   Updated: 2022/09/02 20:27:41 by vimatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
 		return (NULL);
-	while (lst->next)
-	{
+	while (lst->next != NULL)
 		lst = lst->next;
-	}
 	return (lst);
 }
