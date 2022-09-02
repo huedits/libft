7e655b96033261a1907dd038cce89f9e7b5f70fb
vimatheu <vimatheu@student.42sp.org.br>
2022-09-02 21:17:31 +0200

    Automatic commit from Makefile

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: vimatheu <vimatheu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 19:20:17 by vimatheu          #+#    #+#             */
-/*   Updated: 2022/09/02 20:23:40 by vimatheu         ###   ########.fr       */
+/*   Updated: 2022/09/02 21:16:36 by vimatheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	int	i;
+	t_list	*last;
 
 	if (!lst || !new)
 		return ;
-	i = 0;
-	while (lst[i]->next)
-		i++;
-	lst[i]->next = ft_lstnew(new);
+	last = ft_lstlast(*lst);
+	if (!last)
+		*lst = new;
+	else
+		last->next = new;
 }
